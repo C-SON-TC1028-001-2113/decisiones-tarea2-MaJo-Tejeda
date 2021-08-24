a2803d71b3c071b4e72a739169b6e7c2c5784c2e
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-08-24 00:04:52 -0700

    Update ttria.py

--- assignments/03TipoTriangulo/src/ttria.py
+++ assignments/03TipoTriangulo/src/ttria.py
@@ -1,23 +1,23 @@
 def main():
     #escribe tu código abajo de esta línea
     #que no ya lo habiamos hecho en clase 
-    x=int(input("Lado x: "))
-    y=int(input("Lado y: "))
-    z=int(input("Lado z: "))
+    x=int(input("Ingresa la media del Lado 1: "))
+    y=int(input("Ingresa la media del Lado 2: "))
+    z=int(input("Ingresa la media del Lado 3: "))
     
     if x>0 and y>0 and z>0:
       if x+y>z and y+z>x and z+x>y:
         if x==y and x==z:
-            print("Es equilatero")
+            print("ES UN TRIANGULO EQUILATERO")
         elif x==y and x==z or y==z:
-            print("Es isoseles")  
+            print("ES UN TRIANGULO ISOSCELES")  
         else: 
-            print("Es escaleno")  
+            print("ES UN TRIANGULO ESCALENO")  
       else:
-        print("No es un triangulo") 
+        print("NO ES UN TRIANGULO") 
    else:
-     print("No es un triangulo")     
+     print("NO ES UN TRIANGULO")     
             
-            
+
 if __name__=='__main__':
     main()
