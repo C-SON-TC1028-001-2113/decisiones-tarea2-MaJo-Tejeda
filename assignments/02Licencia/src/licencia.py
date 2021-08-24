ab55a7101828eed18be166e6f8e5819fec861c9d
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-08-23 23:47:35 -0700

    Update and rename exercise.py to licencia.py

---
+++ assignments/02Licencia/src/licencia.py
@@ -0,0 +1,23 @@
+def main():
+       edad = int(input("Ingresa tu edad: "))
+        edad >= 18 
+        if edad
+           ID = int(input("¿Tienes identificación oficial? (s/n): "))
+                 if ID == s: 
+                  print ("Trámite de licencia concedido")
+                if ID == n: 
+                    print ("No cumples requisitos")
+                else: 
+                   print ("Respuesta incorrecta") 
+        else: 
+        print ("No cumples requisitos")
+        
+    # Escribe el código adecuado para completar el programa
+    # Para pedir el dato de la idetificación oficial emplea este mensaje:
+    # "¿Tienes identificación oficial? (s/n): "
+    
+
+
+if __name__=='__main__':
+    main()
+
