fed1875a9fb60077d11eea4581c6a41bda5847aa
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-08-24 21:23:47 -0500

    Update licencia.py

--- assignments/02Licencia/src/licencia.py
+++ assignments/02Licencia/src/licencia.py
@@ -1,20 +1,20 @@
 def main():
        edad = int(input("Ingresa tu edad: "))
-        edad >= 18 
-        if edad
-           ID = int(input("¿Tienes identificación oficial? (s/n): "))
-                 if ID == s: 
+       edad >= 18 
+          if edad
+              ID = int(input("¿Tienes identificación oficial? (s/n): "))
+                  if ID == s: 
                   print ("Trámite de licencia concedido")
-                if ID == n: 
-                    print ("No cumples requisitos")
-                else: 
-                   print ("Respuesta incorrecta") 
-        else: 
-        print ("No cumples requisitos")
+                  if ID == n: 
+                  print ("No cumples requisitos")
+              else: 
+              print ("Respuesta incorrecta") 
+          else: 
+          print ("No cumples requisitos")
         
-    # Escribe el código adecuado para completar el programa
-    # Para pedir el dato de la idetificación oficial emplea este mensaje:
-    # "¿Tienes identificación oficial? (s/n): "
+    #Escribe el código adecuado para completar el programa
+    #Para pedir el dato de la idetificación oficial emplea este mensaje:
+    #"¿Tienes identificación oficial? (s/n): "
     
 
 
